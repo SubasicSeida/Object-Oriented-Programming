e0f6c87353cd7010ba63d8bcfe178c624b0bcddc
SubasicSeida <seida.subasic@stu.ibu.edu.ba>
2024-10-18 17:23:31 +0200

    need to connect db

---
+++ src/main/java/Week3/DbConnect.java
@@ -0,0 +1,6 @@
+package Week3;
+
+public class DbConnect {
+    private static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/lab3";
+    private static final String USERNAME =
+}
---
+++ src/main/java/Week3/TaskItem.java
@@ -0,0 +1,34 @@
+package Week3;
+
+public class TaskItem {
+    private int taskId;
+    private String taskDescription;
+    private Status taskStatus;
+
+    public TaskItem(int taskId, String taskDescription, Status taskStatus){
+        this.taskId = taskId;
+        this.taskDescription = taskDescription;
+        this.taskStatus = taskStatus;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+    public void setTaskId(int taskId){
+        this.taskId = taskId;
+    }
+
+    public String getTaskDescription(){
+        return taskDescription;
+    }
+    public void setTaskDescription(String taskDescription){
+        this.taskDescription = taskDescription;
+    }
+
+    public Status getTaskStatus(){
+        return taskStatus;
+    }
+    public void setTaskStatus(Status taskStatus){
+        this.taskStatus = taskStatus;
+    }
+}
