790477250d35066ab4ddd08ac4ec053bfbd92e3a
SubasicSeida <seida.subasic@stu.ibu.edu.ba>
2024-11-10 15:23:49 +0100

    lab4

--- src/main/java/Week4/people/Main.java
+++ src/main/java/Week4/people/Main.java
@@ -12,6 +12,7 @@ public class Main {
         System.out.println(esko);
 
 
+
         Student olli = new Student("Olli", "Ida Albergintie Street 1 00400 Helsinki");
         System.out.println(olli);
         System.out.println("credits " + olli.getCredits());
